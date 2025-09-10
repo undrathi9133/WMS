0f6a2212c6831089c621440bcf74ed3f735dbd4a
UNDRATHI RAVITEJA <155756850+undrathi9133@users.noreply.github.com>
2025-09-10 13:49:06 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,7 +1,11 @@
+import java.util.*;
 public class Add{
 	public static void main(String[] args)
 	{
-		int a=90,b=10;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter numbersa: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
 		int c=a+b;
 		System.out.println(c);
 	}
